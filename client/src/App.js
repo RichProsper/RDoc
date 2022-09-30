ef5c5161eb52c68a91e1a181782e248774bacc2f
Rich Prosper <47926284+RichProsper@users.noreply.github.com>
2022-09-29 21:03:52 -0400

    Implementing router-dom v6

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import TextEditor from './components/TextEditor'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { v4 as uuidV4 } from 'uuid'
 
 function App() {
     return (
         <Router>
-            <TextEditor />
+            <Routes>
+                <Route path="/" element={<Navigate to={`/documents/${uuidV4()}`} />} />
+                <Route path="/documents/:id" element={<TextEditor />} />
+            </Routes>
         </Router>
     )
 }
